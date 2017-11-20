1bd64cd9770a755a931c098aeb91b19c51b5e568
Y.S.LIM <gninraw@lonk.co.kr>
2017-11-20 11:13:14 +0900

    1차 수정본

--- CoffeeApp/CoffeeApp/Model/Coffee.cs
+++ CoffeeApp/CoffeeApp/Model/Coffee.cs
@@ -8,6 +8,12 @@ namespace CoffeeApp.Model
 {
     public class Coffee
     {
-
+        public int CoffeeId { get; set; }
+        public string CoffeeName { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public int StockAmout { get; set; }
+        public DateTime FirstAddedTime { get; set; }
+        public int ImageId { get; set; }
     }
 }
